7f58b2f5171eefb56a3a232c2c9634f6cd7b296a
Tomáš Mîčka <tomas.micka@logmanager.com>
2023-12-16 13:45:24 +0100

    sidebar

--- app/AdminModule/Presenters/BasePresenter.php
+++ app/AdminModule/Presenters/BasePresenter.php
@@ -2,33 +2,39 @@
 
 namespace App\AdminModule\Presenters;
 
+use App\AdminModule\Components\Sidebar\Sidebar;
+use App\AdminModule\Components\Sidebar\SidebarFactory;
 use Nette\Application\AbortException;
 use Nette\Application\ForbiddenRequestException;
+use Nette\Application\UI\Presenter;
+use Nette\DI\Attributes\Inject;
 
-/**
- * Class BasePresenter
- * @package App\AdminModule\Presenters
- */
-abstract class BasePresenter extends \Nette\Application\UI\Presenter {
+abstract class BasePresenter extends Presenter {
 
-  /**
-   * @throws ForbiddenRequestException
-   * @throws AbortException
-   */
-  protected function startup():void {
-    parent::startup();
-    $presenterName = $this->request->presenterName;
-    $action = !empty($this->request->parameters['action'])?$this->request->parameters['action']:'';
+	#[Inject]
+	public SidebarFactory $sidebarFactory;
+	
+	/**
+	 * @throws ForbiddenRequestException
+	 * @throws AbortException
+	 */
+	protected function startup(): void {
+		parent::startup();
+		$presenterName = $this->request->presenterName;
+		$action = !empty($this->request->parameters['action'])?$this->request->parameters['action']:'';
 
-    if (!$this->user->isAllowed($presenterName,$action)){
-      if ($this->user->isLoggedIn()){
-        throw new ForbiddenRequestException();
-      }else{
-        $this->flashMessage('Pro zobrazení požadovaného obsahu se musíte přihlásit!','warning');
-        //uložíme původní požadavek - předáme ho do persistentní proměnné v UserPresenteru
-        $this->redirect(':Front:User:login', ['backlink' => $this->storeRequest()]);
-      }
-    }
-  }
+		if (!$this->user->isAllowed($presenterName,$action)){
+			if ($this->user->isLoggedIn()){
+				throw new ForbiddenRequestException();
+			}else{
+				$this->flashMessage('Pro zobrazení požadovaného obsahu se musíte přihlásit!','warning');
+				$this->redirect(':Front:User:login', ['backlink' => $this->storeRequest()]);
+			}
+		}
+	}
 
+	public function createComponentSidebar(): Sidebar
+	{
+		return $this->sidebarFactory->create();
+	}
 }
